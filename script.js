150e12c1965490d054f02ba43f61a01151f0b9fe
labranka <brankarbc96@gmail.com>
2022-06-24 01:18:57 +0200

    done first page

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+/*Ako je sakriven */
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
